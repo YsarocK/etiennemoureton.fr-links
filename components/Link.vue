2f156e50b757c1b206ba55d84dc42d40d516305e
Etienne <etienne.moureton@unflux.fr>
2022-08-30 11:56:30 +0200

    change title HTML tag

--- components/Link.vue
+++ components/Link.vue
@@ -1,7 +1,7 @@
 <template>
   <a class="w-full rounded-md flex gap-6 px-6 py-4 bg-slate-100" :href="data.url" target="blank">
     <Icon />
-    <h1 class="">{{ data.title }}</h1>
+    <h2>{{ data.title }}</h2>
   </a>
 </template>
 
