a59f6f59e01f553c005ad54212aa1d3cb0703187
Etienne <etienne.moureton@unflux.fr>
2022-09-01 20:20:42 +0200

    fix typescript

--- components/LinkList.vue
+++ components/LinkList.vue
@@ -1,16 +1,21 @@
 <template>
-  <ul class="flex flex-col w-full max-w-lg">
-    <div class="mb-14" v-for="category in categories" :key="category">
+  <ul class="flex flex-col w-full max-w-lg gap-14">
+    <div v-for="category in categories" :key="category">
       <h2 class="mb-4 capitalize font-bold">{{ category }}</h2>
       <ul class="flex flex-col gap-4 w-full max-w-lg">
-        <Link v-for="link in data.filter(el => el.type === category)" :key="link.id" :data="link" />
+        <Link v-for="link in data.filter(el => el.type === category)" :key="link.name" :data="link" />
       </ul>
     </div>
   </ul>
 </template>
 
-<script setup>
-const { data } = await useAsyncData('data', () => queryContent('/links').find())
+<script setup lang="ts">
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+import { Link } from './LinkInterface'
+
+interface Link extends ParsedContent {}
+
+const { data } = await useAsyncData('data', () => queryContent<Link>('/links').find())
 
 const categories = ref([])
 data.value.forEach((el) => {
